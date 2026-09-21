2c0f2abec01cbae19fe02306013da9f613e9ba30
Tonooka01 <lucas.tonooka@outlook.com>
2026-09-21 16:33:53 -0300

    atualizacao

--- static/js/behaviorAnalysis.js
+++ static/js/behaviorAnalysis.js
@@ -3862,7 +3862,7 @@ function _retRenderMainDashboard(d) {
                     <div class="flex-1 bg-green-400 rounded-t opacity-80 hover:opacity-100 hover:bg-green-500 transition-all ret-trend-bar" ${da} data-st="Finalizada" style="height:${pctF}%;min-height:${m.finalizadas?'2px':'0'};" title="Finalizadas: ${m.finalizadas}"></div>
                     <div class="flex-1 bg-orange-400 rounded-t opacity-80 hover:opacity-100 hover:bg-orange-500 transition-all ret-trend-bar" ${da} data-st="Aberta" style="height:${pctA}%;min-height:${abertas?'2px':'0'};" title="Abertas: ${abertas}"></div>
                 </div>
-                <span class="text-[8px] text-gray-400 truncate w-full text-center leading-none mt-0.5 flex-shrink-0">${mm}/${yy.slice(2)}</span>
+                <span class="text-[11px] font-semibold text-gray-700 truncate w-full text-center leading-none mt-0.5 flex-shrink-0">${mm}/${yy.slice(2)}</span>
             </div>`;
         }).join('');
     };
